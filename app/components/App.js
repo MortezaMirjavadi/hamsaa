0074e7205071bb097bdfe0015bfaa1d97af5cf5c
Mirjavadi <morteza.mirjavadi@yahoo.com>
2022-01-27 00:00:31 +0330

    change logo.

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,11 @@
-import React, {Suspense, useEffect} from "react";
+import React, {Suspense} from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
 import routes from "@Routes/index";
 import PropTypes from "prop-types";
 import Loading from "./Loading/Loading";
 import Snackbar from "@Components/UI/Snackbar";
-import {DispatchContext, GlobalContext} from "@Store/globalContext";
-import {ACTION_SHOW_SNACKBAR} from "@Store/actionTypes";
+import {GlobalContext} from "@Store/globalContext";
 
 function App() {
   const x = document.getElementById("splash");
@@ -14,11 +13,6 @@ function App() {
     document.body.removeChild(x);
   }
   const { isShowSnackbar } = React.useContext(GlobalContext);
-  // const dispatch = React.useContext(DispatchContext);
-
-  // useEffect(() => {
-  //   dispatch({type: ACTION_SHOW_SNACKBAR, message: "this is a test"});
-  // }, []);
 
   return (
     <>
